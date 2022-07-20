cedb6ad7e5cfcb4b1af4202e9ffcfddcf2cf11d8
Dmitry Dudin <dmitrodonskoy@yandex.ru>
2022-07-20 20:41:22 +0300

    Чистка от комментариев

--- webpack.config.js
+++ webpack.config.js
@@ -18,31 +18,22 @@ module.exports = {
     open: true
   },
   module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
       },
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
       {
-        // применять это правило только к CSS-файлам
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
           options: { importLoaders: 1 }
         },
-          // Добавьте postcss-loader
           'postcss-loader']
       },
 
@@ -50,7 +41,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // путь к файлу index.html
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
